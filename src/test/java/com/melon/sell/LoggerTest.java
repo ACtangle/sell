af22d6518a0dbc36e3698b45f243d306812631b5
actangle <674253854@qq.com>
2019-07-01 14:10:56 +0800

    create

---
+++ src/test/java/com/melon/sell/LoggerTest.java
@@ -0,0 +1,22 @@
+package com.melon.sell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoggerTest {
+
+    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+
+    @Test
+    public void test() {
+        logger.debug("debug111");
+        logger.info("info111");
+        logger.error("error11");
+    }
+}
